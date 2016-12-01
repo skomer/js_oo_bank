080f031f9edd3cf984a7e0346e0c5d2469f2ec79
Josephine Elder <parusmontanus@gmail.com>
2016-12-01 14:32:56 +0000

    Wrote test for Bank finding an account by name

--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -8,10 +8,13 @@ describe('Bank', function() {
     var thingBotsBank;
     var vault;
     var accountOfAdrian;
+    var accountOfJo;
 
     beforeEach(function() {
         thingBotsBank = new Bank("ThingBot's");
-        accountOfAdrian = new Account("Adrian's account", 'business', 600000000.14);
+        accountOfAdrian = new Account("Adrian's account", 'business', 600000.14);
+        accountOfJo = new Account("Jo's account", 'personal', 0.30);
+        accountOfJohn = new Account("John's account", 'business', 4764000000.99);
     });
 
     it('has a name', function() {
@@ -23,6 +26,12 @@ describe('Bank', function() {
     it('Bank can add an account to its vault', function() {
         thingBotsBank.add(accountOfAdrian);
         assert.equal(1, thingBotsBank.vault.length);
+    });
+    it('Bank can find an account by name', function() {
+        var foundAccount = thingBotsBank.find("accountOfJo");
+        assert.equal("Jo's account", foundAccount.name);
+        assert.equal(0.30, foundAccount.amount);
+        assert.equal('personal', foundAccount.type);
     });
 
 
